8fce26bc6311c124262f24ccc9cd1c599267ae72
Jesse Boyer <aniyishay@gmail.com>
2015-03-07 02:02:32 -0500

    Part 2

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,45 @@ var gulp        = require("gulp"),
     plumber     = require("gulp-plumber"),
     minify_css  = require("gulp-minify-css"),
     uglify      = require("gulp-uglify"),
-    autoprefixer= require("autoprefixer");
+    sourcemaps  = require("gulp-sourcemaps"),
+    notify      = require("gulp-notify"),
+    prefix      = require("gulp-autoprefixer"),
+    imagemin    = require("gulp-imagemin"),
+    pngquant    = require("imagemin-pngquant");
 
 // --------------------------------------------------------------------
 
 var dest_js  = "dist/js";
 var dest_css = "dist/css";
-var src_sass = "src/sass";
-var src_js   = "src/js";
+var dest_img = "dist/img";
+
+var src_sass = "src/sass/**/*.scss";
+var src_js   = "src/js/**/*.js";
+var src_img   = "src/img/*";
 
 // --------------------------------------------------------------------
+
+var onError = function(err) {
+    console.log(err);
+    this.emit('end');
+};
 
 // SASS to CSS
 gulp.task('sass', function() {
 
-    gulp.src(src_sass)
-        .pipe(plumber())
+    return gulp.src(src_sass)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(concat('app.min.css'))
         .pipe(gulp.dest(dest_css))
         .pipe(minify_css())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(dest_css));
+        // .pipe(notify({message: 'Hello world we are done'}));
 });
 
 // --------------------------------------------------------------------
@@ -33,10 +52,48 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
 
-    gulp.src(src_js)
-        .pipe(plumber())
+    return gulp.src(src_js)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(uglify())
         .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(dest_js));
 
 });
+
+// --------------------------------------------------------------------
+
+// Images
+
+gulp.task('img', function() {
+
+    return gulp.src(src_img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(dest_img));
+
+});
+
+// --------------------------------------------------------------------
+
+// Watch
+
+gulp.task('watch', function() {
+    gulp.watch(src_js, ['js']);
+    gulp.watch(src_sass, ['sass']);
+    gulp.watch(src_img, ['img']);
+});
+
+
+// --------------------------------------------------------------------
+
+// Default
+
+gulp.task('default', ['watch', 'sass', 'js', 'img']);
+
